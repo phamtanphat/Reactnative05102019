2fb31867e4c4623bd62e66a7fa2ca73dfd409402
phamtanphat <phatdroid94@gmail.com>
2019-10-26 00:03:30 -0700

    bai tap 1

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
 class App extends Component {
   render() {
     return (
-      <View>
-        <Text
-          style={{
-            backgroundColor: 'blue',
-            fontSize: 20,
-            fontStyle: 'italic',
-            fontWeight: '700',
-            padding: 50,
-          }}>
-          Hello react
-        </Text>
-        <Text>react native</Text>
+      <View style={{flex : 1,flexDirection: 'column'}}>
+        <View style={{flex : 1, backgroundColor : 'red', flexDirection : 'column'}}>
+            <Text style={{flex : 1 , backgroundColor : 'yellow' , color : 'black', textAlign: 'center' , textAlignVertical: 'center'}}>A</Text>
+            <Text style={{flex : 1 , backgroundColor : 'green' , color : 'black', textAlign: 'center' , textAlignVertical: 'center'}}>B</Text>
+            <Text style={{flex : 1 , backgroundColor : 'gray' , color : 'black', textAlign: 'center' , textAlignVertical: 'center'}}>C</Text>
+            <Text style={{flex : 1 , backgroundColor : 'pink' , color : 'black', textAlign: 'center' , textAlignVertical: 'center'}}>D</Text>
+        </View>
+        <View style={{flex : 1, backgroundColor : 'blue' }}>
+          
+        </View>
       </View>
     );
   }
