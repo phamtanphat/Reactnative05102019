92475f3aa52ae30dfd6cbc8c6ed0343f365e09bb
phamtanphat <phatdroid94@gmail.com>
2019-10-26 00:08:24 -0700

    finish bai tap 1

--- App.js
+++ App.js
@@ -5,15 +5,91 @@ import {View, Text} from 'react-native';
 class App extends Component {
   render() {
     return (
-      <View style={{flex : 1,flexDirection: 'column'}}>
-        <View style={{flex : 1, backgroundColor : 'red', flexDirection : 'column'}}>
-            <Text style={{flex : 1 , backgroundColor : 'yellow' , color : 'black', textAlign: 'center' , textAlignVertical: 'center'}}>A</Text>
-            <Text style={{flex : 1 , backgroundColor : 'green' , color : 'black', textAlign: 'center' , textAlignVertical: 'center'}}>B</Text>
-            <Text style={{flex : 1 , backgroundColor : 'gray' , color : 'black', textAlign: 'center' , textAlignVertical: 'center'}}>C</Text>
-            <Text style={{flex : 1 , backgroundColor : 'pink' , color : 'black', textAlign: 'center' , textAlignVertical: 'center'}}>D</Text>
+      <View style={{flex: 1, flexDirection: 'column'}}>
+        <View
+          style={{flex: 1, backgroundColor: 'red', flexDirection: 'column'}}>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'yellow',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            A
+          </Text>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'green',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            B
+          </Text>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'gray',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            C
+          </Text>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'pink',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            D
+          </Text>
         </View>
-        <View style={{flex : 1, backgroundColor : 'blue' }}>
-          
+        <View style={{flex: 1, backgroundColor: 'blue', flexDirection: 'row'}}>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'yellow',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            A
+          </Text>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'green',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            B
+          </Text>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'gray',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            C
+          </Text>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'pink',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            D
+          </Text>
         </View>
       </View>
     );
