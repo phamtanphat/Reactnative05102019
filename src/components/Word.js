35a71265e1adbafe707cecb7e462a0a820221285
phamtanphat <phatdroid94@gmail.com>
2019-10-26 19:29:30 -0700

    feat<Word> : css ui

--- src/components/Word.js
+++ src/components/Word.js
@@ -11,14 +11,19 @@ export default class Word extends Component {
       <View
         style={{
           flexDirection: 'column',
-          height: width / 5,
+          height: width / 4,
           borderColor: 'white',
           borderRadius: width / 100,
           backgroundColor: 'white',
           margin: width / 70,
           justifyContent: 'center',
         }}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            paddingHorizontal: width / 8,
+          }}>
           <Text style={{fontSize: width / 15, color: '#28a745'}}> {en} </Text>
           <Text style={{fontSize: width / 15, color: 'red'}}>
             {isMemorized ? '----' : vn}
@@ -27,14 +32,30 @@ export default class Word extends Component {
         <View
           style={{
             flexDirection: 'row',
-            justifyContent: 'space-around',
+            justifyContent: 'space-between',
             marginTop: width / 30,
+            paddingHorizontal: width / 15,
           }}>
-          <TouchableOpacity>
-            <Text>{isMemorized ? 'Forgot' : 'isMemorized'}</Text>
+          <TouchableOpacity
+            style={{
+              backgroundColor: isMemorized ? '#28a745' : 'red',
+              padding: width / 65,
+              marginLeft: isMemorized ? width / 20 : 0,
+            }}>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: width / 20,
+              }}>
+              {isMemorized ? 'Forgot' : 'isMemorized'}
+            </Text>
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Text>Remove</Text>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#E0A800',
+              padding: width / 65,
+            }}>
+            <Text style={{color: 'white', fontSize: width / 20}}>Remove</Text>
           </TouchableOpacity>
         </View>
       </View>
