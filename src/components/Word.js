32233c18e31018d7022c6dfa8bee67808e5b7fed
phamtanphat <phatdroid94@gmail.com>
2019-10-26 20:04:30 -0700

    feat<Word> : css finish

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,69 +1,79 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {Text, View, TouchableOpacity, Dimensions} from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
 import {Dimensionapp} from '../unit/Dimensionapp';
 
 export default class Word extends Component {
   render() {
     const {en, vn, isMemorized} = this.props.word;
     return (
-      <View
-        style={{
-          flexDirection: 'column',
-          height: Dimensionapp.getWidth() / 4,
-          borderColor: 'white',
-          borderRadius: Dimensionapp.getWidth() / 100,
-          backgroundColor: 'white',
-          margin: Dimensionapp.getWidth() / 70,
-          justifyContent: 'center',
-        }}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            paddingHorizontal: Dimensionapp.getWidth() / 8,
-          }}>
-          <Text
-            style={{fontSize: Dimensionapp.getWidth() / 15, color: '#28a745'}}>
-            {en}
-          </Text>
-          <Text style={{fontSize: Dimensionapp.getWidth() / 15, color: 'red'}}>
-            {isMemorized ? '----' : vn}
-          </Text>
+      <View style={styles.container}>
+        <View style={styles.groupText}>
+          <Text style={styles.textEn}>{en}</Text>
+          <Text style={styles.textVn}>{isMemorized ? '----' : vn}</Text>
         </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            marginTop: Dimensionapp.getWidth() / 30,
-            paddingHorizontal: Dimensionapp.getWidth() / 15,
-          }}>
+        <View style={styles.groupButton}>
           <TouchableOpacity
             style={{
+              ...styles.touchableMemorized,
               backgroundColor: isMemorized ? '#28a745' : 'red',
-              padding: Dimensionapp.getWidth() / 65,
               marginLeft: isMemorized ? Dimensionapp.getWidth() / 20 : 0,
             }}>
-            <Text
-              style={{
-                color: 'white',
-                fontSize: Dimensionapp.getWidth() / 20,
-              }}>
+            <Text style={styles.textMemorized}>
               {isMemorized ? 'Forgot' : 'isMemorized'}
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              backgroundColor: '#E0A800',
-              padding: Dimensionapp.getWidth() / 65,
-            }}>
-            <Text
-              style={{color: 'white', fontSize: Dimensionapp.getWidth() / 20}}>
-              Remove
-            </Text>
+          <TouchableOpacity style={styles.touchableRemove}>
+            <Text style={styles.textRemove}>Remove</Text>
           </TouchableOpacity>
         </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    height: Dimensionapp.getWidth() / 4,
+    borderColor: 'white',
+    borderRadius: Dimensionapp.getWidth() / 100,
+    backgroundColor: 'white',
+    margin: Dimensionapp.getWidth() / 70,
+    justifyContent: 'center',
+  },
+  groupText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: Dimensionapp.getWidth() / 8,
+  },
+  textVn: {
+    fontSize: Dimensionapp.getWidth() / 15,
+    color: 'red',
+  },
+  textEn: {
+    fontSize: Dimensionapp.getWidth() / 15,
+    color: '#28a745',
+  },
+  groupButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: Dimensionapp.getWidth() / 30,
+    paddingHorizontal: Dimensionapp.getWidth() / 15,
+  },
+  touchableMemorized: {
+    padding: Dimensionapp.getWidth() / 65,
+  },
+  textMemorized: {
+    color: 'white',
+    fontSize: Dimensionapp.getWidth() / 20,
+  },
+  touchableRemove: {
+    backgroundColor: '#E0A800',
+    padding: Dimensionapp.getWidth() / 65,
+  },
+  textRemove: {
+    color: 'white',
+    fontSize: Dimensionapp.getWidth() / 20,
+  },
+});
