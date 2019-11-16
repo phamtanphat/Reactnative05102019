0ecef559b7ca5df44852441cc958d59d766b9df8
phamtanphat <phatdroid94@gmail.com>
2019-11-16 00:38:19 -0800

    reducer

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import List from './src/components/List';
 import MyState from './src/components/MyState';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import { combineReducers } from 'redux';
+
 
 /* Create store */
 // 1 : Tao ra store (Noi chua du lieu cua app can chia se);
@@ -38,33 +40,31 @@ const defaultState = {
   filterMode: 'Show_All',
 };
 
-const store = createStore((state = defaultState, action) => {
-  if (action.type === 'TOGGLE_WORD') {
-    const newWords = state.words.map(item => {
-      if (action.id === item.id) {
-        return {...item, isMemorized: !item.isMemorized};
-      }
-      return item;
-    });
-    return {...state, words: newWords};
-  }
-  if (action.type === 'REMOVE_WORD') {
-    const newWords = state.words.filter(item => action.id !== item.id);
-    return {...state, words: newWords};
-  }
-  if (action.type === 'TOGGLE_FORM') {
-    return {...state, shouldShowForm: !state.shouldShowForm};
-  }
-  if (action.type === 'ADD_WORD') {
-    const coppyWords = Object.assign([], state.words);
-    coppyWords.unshift(action.word);
-    return {...state, words: coppyWords};
-  }
-  if (action.type === 'SET_FILTER_MODE') {
-    return {...state, filterMode: action.filterMode};
-  }
+const defaultWords = [
+  {id: 'a1', en: 'One', vn: 'Mot', isMemorized: true},
+  {id: 'a2', en: 'Two', vn: 'Hai', isMemorized: false},
+  {id: 'a3', en: 'Three', vn: 'Ba', isMemorized: false},
+  {id: 'a4', en: 'Four', vn: 'Bon', isMemorized: false},
+  {id: 'a5', en: 'Five', vn: 'Nam', isMemorized: true},
+];
+
+function reducerWords(state = defaultWords, action) {
   return state;
+}
+function reducershouldShowForm(state = false, action) {
+  return state;
+}
+function reducerfilterMode(state = 'Show_All', action) {
+  return state;
+}
+
+const reducer = combineReducers({
+  words: reducerWords,
+  shouldShowForm: reducershouldShowForm,
+  filterMode: reducerfilterMode,
 });
+
+const store = createStore(reducer);
 
 class App extends Component {
   render() {
